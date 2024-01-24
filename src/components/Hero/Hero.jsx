96e62eb13ddbc3c017654c73b594a57d8a0fd821
Aman Dixit <aman.d6t@gmail.com>
2024-01-24 23:51:09 +0530

    Hero Section added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -35,7 +35,38 @@ const Hero = () => {
     >
       {/* background pattern */}
       <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-9"></div>
-      <div>{/* hero section */}</div>
+      {/* Hero Section */}
+      <div className="container pb-8 sm:pb-0">
+        <div>
+          <div grid grid-cols-1 sm:grid-cols-2>
+            {/* text content section */}
+            <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10 ">
+              <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">
+                Lorem ipsum dolor sit.
+              </h1>
+              <p className="text-sm">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae,
+                ad?
+              </p>
+              <div>
+                <button className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
+                  Order Now
+                </button>
+              </div>
+            </div>
+            {/* Image Section */}
+            <div className="order-1 sm:order-2">
+              <div className="relative z-10">
+                <img
+                  src={Image1}
+                  alt=""
+                  className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125 object-contain mx-auto"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
