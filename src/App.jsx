2b84423dccd890b567a140d332bcb832df922cb0
Aman Dixit <aman.d6t@gmail.com>
2024-01-26 00:50:56 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import Product from "./components/Products/Product";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import TopProduct from "./components/TopProducts.jsx/TopProduct";
+import Banner from "./components/Banner/Banner";
+import Subscribe from "./components/Subscribe/Subscribe";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
   return (
     <div>
       <Navbar />
       <Hero />
+      <Product />
+      <TopProduct />
+      <Banner />
+      <Subscribe />
+      <Product />
+      <Testimonials />
+      <Footer />
     </div>
   );
 };
